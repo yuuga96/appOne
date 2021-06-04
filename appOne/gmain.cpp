c610148cb9b8f534860d105e0e27030898a5764f
yuuga <yuuga@MSI>
2021-06-04 12:32:10 +0900

    ani

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,209 +1,174 @@
-#if 1
+#define _EX
+#ifdef _EX
+
 #include"libOne.h"
-
-struct DATA {
-    int INIT = 0;
-    int PLAY = 1;
-    int RESULT = 2;
-    int state = INIT;
-};
-
-void loadImages(struct DATA* d) {}
-void init(struct DATA* d) {}
-void play(struct DATA* d) {}
-void result(struct DATA* d) {}
-void draw(struct DATA* d) {}
 
 void gmain() {
-    window(800, 450);
-    struct DATA d;
-    loadImages(&d);
+    window(1920, 1080, full);
+    //爆発用データ
+    struct EXPLOSION {
+        float px, py, angle, scale;
+        int interval, counter = -1;
+    };
+    const int numExplosions = 30;
+    struct EXPLOSION e[numExplosions];
+    int triggerCnt = 0;
+    int triggerInterval = 10;
+    //画像データ
+    const  int numImgs = 60;
+    int imgs[numImgs];
+    for (int i = 0; i < numImgs; i++) {
+        char filename[32];
+        sprintf_s(filename, "assets\\explosion\\a%02d.png", i);
+        imgs[i] = loadImage(filename);
+    }
+
     while (notQuit) {
-        if      (d.state == d.INIT  ) { init(&d); }
-        else if (d.state == d.PLAY  ) { play(&d); }
-        else if (d.state == d.RESULT) { result(&d); }
-        draw(&d);
-    }
-}
 
-#else
-
-#include"libOne.h"
-
-struct HAND {
-    int hand;
-    int img[3];
-    int heartImg;
-    float px;
-    float py;
-    struct COLOR color;
-    float angle;
-    int life;
-};
-
-struct RESULT_TEXT {
-    const char* str;
-    float px;
-    float py;
-    float size;
-    struct COLOR color;
-};
-
-struct DATA {
-    int INIT = 0;
-    int PLAY = 1;
-    int RESULT = 2;
-    int state = INIT;
-
-    struct COLOR pink = { 255,200,200 };
-    struct COLOR white = { 255,255,255 };
-    struct COLOR red = { 255,0,0 };
-    struct COLOR blue = { 0,0,200 };
-    int GU = 0;
-    int CHOKI = 1;
-    int PA = 2;
-    struct HAND player;
-    struct HAND pc;
-    struct RESULT_TEXT resultText;
-};
-
-void drawHands(struct DATA* d);
-void drawResultText(struct DATA* d);
-
-void loadImages(struct DATA* d) {
-    d->player.img[d->GU] = loadImage("assets\\playerGu.png");
-    d->player.img[d->CHOKI] = loadImage("assets\\playerChoki.png");
-    d->player.img[d->PA] = loadImage("assets\\playerPa.png");
-    d->player.heartImg = loadImage("assets\\heart.png");
-    d->pc.img[d->GU] = loadImage("assets\\pcGu.png");
-    d->pc.img[d->CHOKI] = loadImage("assets\\pcChoki.png");
-    d->pc.img[d->PA] = loadImage("assets\\pcPa.png");
-    d->pc.heartImg = d->player.heartImg;
-}
-void init(struct DATA* d) {
-    d->player.hand = d->GU;
-    d->player.px = 250;
-    d->player.py = 225;
-    d->player.color = d->white;
-    d->player.life = 3;
-    d->player.angle = 0;
-
-    d->pc.hand = d->GU;
-    d->pc.px = 550;
-    d->pc.py = 225;
-    d->pc.color = d->white;
-    d->pc.life = 3;
-    d->pc.angle = 0;
-
-    d->resultText.str = "勝ち";
-    d->resultText.color = d->red;
-    d->resultText.px = 255;
-    d->resultText.py = 320;
-    d->resultText.size = 0;
-    
-    drawHands(d);
-
-    //ステート切り替え
-    d->state = d->PLAY;
-}
-void play(struct DATA* d) {
-    if (!isTrigger(KEY_A) && !isTrigger(KEY_S) && !isTrigger(KEY_D)) {
-        return;
-    }
-    //プレイヤーの手
-    if (isTrigger(KEY_A)) { d->player.hand = d->GU; }
-    if (isTrigger(KEY_S)) { d->player.hand = d->CHOKI; }
-    if (isTrigger(KEY_D)) { d->player.hand = d->PA; }
-    //ＰＣの手
-    d->pc.hand = random() % 3;
-    //結果判定
-    if (d->player.hand == d->pc.hand) {
-        //あいこ
-        d->player.color = d->white;
-        d->pc.color = d->white;
-    }
-    else if ((d->player.hand + 1) % 3 == d->pc.hand) {
-        //プレイヤー勝ち
-        d->pc.life--;
-        d->player.color = d->pink;
-        d->pc.color = d->white;
-    }
-    else {
-        //ＰＣ勝ち
-        d->player.life--;
-        d->player.color = d->white;
-        d->pc.color = d->pink;
-    }
-    //描画
-    drawHands(d);
-    //ステート切り替え
-    if (d->player.life == 0 || d->pc.life == 0) {
-        if (d->player.life == 0) {
-            d->resultText.str = "負け";
-            d->resultText.color = d->blue;
+        //爆発トリガー
+        ++triggerCnt %= triggerInterval;
+        if (triggerCnt == 0) {
+            for (int i = 0; i < numExplosions; i++) {
+                if (e[i].counter == -1) {
+                    e[i].px = random(width / 2 - 200,width / 2 +200);
+                    e[i].py = random(height / 2 - 100,height / 2 +100);
+                    e[i].angle = random(6.28f);
+                    e[i].scale = random(5,10);
+                    e[i].interval = random(1,3);
+                    e[i].counter = 0;
+                    i = numExplosions;
+                }
+            }
         }
-        d->state = d->RESULT;
-    }
-}
-void result(struct DATA* d) {
-    //負けた手が回転しながら落ちていく
-    if (d->player.life == 0) {
-        d->player.py += 1;
-        d->player.angle += 0.003f;
-    }
-    else {
-        d->pc.py += 1;
-        d->pc.angle += -0.003f;
-    }
-    //結果文字拡大
-    if (d->resultText.size < 180) {
-        d->resultText.size += 10;
-    }
-    //描画
-    drawHands(d);
-    drawResultText(d);
-    //ステート切り替え
-    if (isTrigger(KEY_SPACE)) {
-        d->state = d->INIT;
+        //爆発進行
+        for (int i = 0; i < numExplosions; i++) {
+            if (e[i].counter >= 0) {
+                e[i].counter++;
+                //爆発終了
+                if (e[i].counter >= e[i].interval * numImgs) {
+                    e[i].counter = -1;
+                }
+            }
+        }
+        //描画
+        clear();
+        for (int i = 0; i < numExplosions; i++) {
+            if (e[i].counter >= 0) {
+                rectMode(CENTER);
+                int no = e[i].counter / e[i].interval;
+                image(imgs[no], e[i].px, e[i].py, e[i].angle, e[i].scale);
+            }
+        }
     }
 }
 
-void drawHand(struct HAND* hand) {
-    //手
-    rectMode(CENTER);
-    imageColor(hand->color);
-    int i = hand->hand;
-    image(hand->img[i], hand->px, hand->py, hand->angle);
-    //ハート
-    imageColor(255, 0, 0);
-    for (i = 0; i < hand->life; i++) {
-        image(hand->heartImg, hand->px + 50 * (i - 1), hand->py - 100);
-    }
-}
-void drawHands(struct DATA* d) {
-    clear(180);
-    drawHand(&d->player);
-    drawHand(&d->pc);
-}
-void drawResultText(struct DATA* d) {
-    textSize(d->resultText.size);
-    fill(d->resultText.color);
-    text(d->resultText.str, d->resultText.px, d->resultText.py);
-}
+#endif
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef _SAMPLE1
+#include"libOne.h"
 void gmain() {
-    //ウィンドウ表示
-    window(800, 450);
-    //データ用意
-    struct DATA d;
+    window(1920, 1080, full);
+    //キャラデータ
+    float px = width / 2, py = height / 2, angle = 0, scale = 1.0f;
+    int counter = -1, interval = 8;
+    //画像番号配列データ
+    const int numImgs = 8;
+    int imgs[numImgs];
     //画像読み込み
-    loadImages(&d);
-    //メインループ
-    while (notQuit) {
-        //ステート制御
-        if      (d.state == d.INIT  ) { init(&d); }
-        else if (d.state == d.PLAY  ) { play(&d); }
-        else if (d.state == d.RESULT) { result(&d); }
+    for (int i = 0; i < numImgs; i++) {
+        char filename[32];
+        sprintf_s(filename, "assets\\lady\\a%d.png", i);
+        imgs[i] = loadImage(filename);
     }
+    //メインループ
+    ShowCursor(false);
+    while (notQuit) {
+        int maxFrame = interval * numImgs;
+        ++counter %= maxFrame;
+        int no = counter / interval;
+        //描画
+        clear(200, 160, 200);
+        rectMode(CENTER);
+        image(imgs[no], px, py, angle, scale);
+    }
+    ShowCursor(true);
+}
+#endif
+
+#ifdef _SAMPLE2
+#include"libOne.h"
+void gmain() {
+    window(1920, 1080, full);
+    //爆発データ
+    struct EXPLOSION {
+        float px, py, angle, scale;
+        int interval, counter = -1;
+    };
+    const int numExplosions = 30;
+    struct EXPLOSION e[numExplosions];
+    int triggerCnt = 0;
+    int triggerInterval = 5;
+    //画像データ
+    const int numImgs = 60;
+    int imgs[numImgs];
+    for (int i = 0; i < numImgs; i++) {
+        char filename[32];
+        sprintf_s(filename, "assets\\explosion\\a%02d.png", i);
+        imgs[i] = loadImage(filename);
+    }
+    ShowCursor(false);
+    while (notQuit) {
+        //爆発開始
+        ++triggerCnt %= triggerInterval;
+        if (triggerCnt == 0) {
+            for (int i = 0; i < numExplosions; i++) {
+                if (e[i].counter == -1) {
+                    e[i].px = random(width / 2 - 150, width / 2 + 150);
+                    e[i].py = random(height / 2 - 100, height / 2 + 100);
+                    e[i].angle = random(3.14f * 2);
+                    e[i].scale = random(5.0f, 10.0f);
+                    e[i].interval = random(1, 3);
+                    e[i].counter = 0;
+                    i = numExplosions;
+                }
+            }
+        }
+        //爆発進行
+        for (int i = 0; i < numExplosions; i++) {
+            if (e[i].counter >= 0) {
+                ++e[i].counter;
+                //爆発終了
+                if (e[i].counter >= e[i].interval * numImgs) {
+                    e[i].counter = -1;
+                }
+            }
+        }
+        //描画
+        clear(60, 120, 240);
+        rectMode(CENTER);
+        for (int i = 0; i < numExplosions; i++) {
+            if (e[i].counter >= 0) {
+                int no = e[i].counter / e[i].interval;
+                image(imgs[no], e[i].px, e[i].py, e[i].angle, e[i].scale);
+            }
+        }
+    }
+    ShowCursor(true);
 }
 #endif
